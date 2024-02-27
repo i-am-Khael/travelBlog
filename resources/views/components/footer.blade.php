9f0c4c3510d64414414acda0ad7d4920f24d171f
i-am-Khael <casalsmichael@protonmail.com>
2024-02-27 17:54:07 +0800

    TravelBlog

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer class="w-full " >
+    <div class="w-[90%] max-w-[90rem] mx-auto py-4 flex items-center justify-between" >
+        <p>&copy; TravelBlog 2024. All right reserved.</p>
+        <p>Casals, Michael</p>
+    </div>
+</footer>
---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,10 @@
+
+<nav class="w-full shadow-xl sticky top-0 bg-gray-900 text-gray-50" >
+    <div class="w-[90%] max-w-[90rem] mx-auto py-2" >
+
+        <h1>
+            <a href="/">TravelBlog</a>
+        </h1>
+
+    </div>
+</nav>
