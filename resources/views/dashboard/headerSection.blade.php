be93a6a2988dcef03c702e9ae7c3301f000944c1
i-am-Khael <casalsmichael@protonmail.com>
2024-03-09 08:21:56 +0800

    03-09-2024

--- resources/views/dashboard/headerSection.blade.php
+++ resources/views/dashboard/headerSection.blade.php
@@ -4,44 +4,46 @@
 @section('content')
 <section class="w-full h-screen p-4 overflow-scroll" >
 
-    <div class="w-full flex items-center justify-between">
-        <h2>Contents</h2>
-        <a href="{{route('header.create')}}">Add</a>
+    <div class="w-full p-3 rounded-md flex items-center justify-between bg-gray-900">
+        <h2 class="text-xl max-sm:text-base uppercase tracking-wide font-semibold" >Header Section</h2>
+        <a href="{{route('header.create')}}"
+            class="px-4 py-2 rounded-md bg-green-700 hover:bg-green-600 text-gray-50" >Add</a>
     </div>
 
-    <x-flashMessage/>
+    <x-flashMessage class="my-3" />
 
-    <div class="w-full overflow-x-scroll" >
-        <table class="w-full">
-            <thead>
+    <div class="w-full my-12 overflow-x-scroll" >
+        <table class="w-full table-auto">
+            <thead class="border-b-[1px] border-gray-50/5" >
                 <tr class="text-left" >
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Image</th>
-                    <th>Published</th>
-                    <th>Action</th>
+                    <th class="p-3 text-left" >Title</th>
+                    <th class="p-3 text-left" >Description</th>
+                    <th class="p-3 text-left" >Image</th>
+                    <th class="p-3 text-left" >Published</th>
+                    <th class="p-3 text-left" >Action</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($headerContent as $content )
-                <tr>
-                    <td>{{ $content->title }}</td>
-                    <td class="text-wrap line-clamp-1">{{ $content->desc }}</td>
-                    <td>
+                <tr class="border-b-[1px] border-gray-50/5" >
+                    <td class="p-3" >{{ $content->title }}</td>
+                    <td class="p-3 text-wrap line-clamp-1">{{ $content->desc }}</td>
+                    <td class="p-3" >
                         <img src="{{asset('storage/'.$content->image)}}" alt="image"
                             class="w-[100px] h-[50px]">
                     </td>
-                    <td>
+                    <td class="p-3" >
                         <form id="form{{$content->id}}" action="{{ route('header.updatePublished', $content->id) }}" method="POST" >
                             @csrf
                             <input name="isPublished" type="checkbox" {{ $content->isPublished ? 'checked' : '' }} onchange="submitForm({{$content->id}})" >
                         </form>
                     </td>
-                    <td class="flex gap-4">
-                        <a href="{{ route('header.edit', $content->id) }}">Edit</a>
+                    <td class="p-3 flex gap-4">
+                        <a href="{{ route('header.edit', $content->id) }}" class="px-4 py-2 rounded-md
+                            bg-blue-700 hover:bg-blue-600 text-gray-50" >Edit</a>
                         <form action="{{ route('header.delete', $content->id) }}" method="POST">
                             @method('delete') @csrf
-                            <button>Delete</button>
+                            <button class="px-4 py-2 rounded-md bg-red-700 hover:bg-red-600 text-gray-50" >Delete</button>
                         </form>
                     </td>
                 </tr>
